971e2e08a2a2d365feb3f468bb972c42fae64e61
GongSulMin <tv302@naver.com>
2019-12-29 23:12:48 +0900

    Register 기능 추가

---
+++ app/src/main/java/com/gong/daggercodelab/registration/RegistrationActivity.kt
@@ -0,0 +1,48 @@
+package com.gong.daggercodelab.registration
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gong.daggercodelab.MyApplication
+import com.gong.daggercodelab.R
+import com.gong.daggercodelab.main.MainActivity
+import com.gong.daggercodelab.registration.enterdetails.EnterDetailsFragment
+import com.gong.daggercodelab.registration.termsandconditions.TermsAndConditionsFragment
+
+class RegistrationActivity : AppCompatActivity() {
+
+    lateinit var registrationViewModel: RegistrationViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+        registrationViewModel = RegistrationViewModel((application as MyApplication).userManager)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_holder, EnterDetailsFragment())
+            .commit()
+    }
+
+    fun onDetailsEntered() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_holder, TermsAndConditionsFragment())
+            .addToBackStack(TermsAndConditionsFragment::class.java.simpleName)
+            .commit()
+    }
+
+    fun onTermsAndConditionsAccepted() {
+        registrationViewModel.registerUser()
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            supportFragmentManager.popBackStack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/gong/daggercodelab/registration/RegistrationViewModel.kt
@@ -0,0 +1,28 @@
+package com.gong.daggercodelab.registration
+
+import androidx.lifecycle.ViewModel
+import com.gong.daggercodelab.data.UserManager
+
+class RegistrationViewModel(val userManager: UserManager) : ViewModel() {
+
+    private var userName: String? = null
+    private var password: String? = null
+    private var acceptedTcs: Boolean? = null
+
+    fun updateUserData(userName: String, password: String) {
+        this.userName = userName
+        this.password = password
+    }
+
+    fun acceptTCs() {
+        acceptedTcs = true
+    }
+
+    fun registerUser() {
+        assert(userName != null)
+        assert(password != null)
+        assert(acceptedTcs == true)
+        userManager.registerUser(userName!!, password!!)
+    }
+
+}
