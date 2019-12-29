4fcdb64c8aae9fe83adeb65da4f48abd5944921d
GongSulMin <tv302@naver.com>
2019-12-30 00:22:36 +0900

    Dagger Inject For Application

--- app/src/main/java/com/gong/daggercodelab/MyApplication.kt
+++ app/src/main/java/com/gong/daggercodelab/MyApplication.kt
@@ -3,8 +3,14 @@ package com.gong.daggercodelab
 import android.app.Application
 import com.gong.daggercodelab.data.UserManager
 import com.gong.daggercodelab.data.storage.SharedPreferencesStorage
+import com.gong.daggercodelab.di.AppComponent
+import com.gong.daggercodelab.di.DaggerAppComponent
 
 class MyApplication : Application() {
+
+    val appComponent: AppComponent by lazy {
+        DaggerAppComponent.factory().create(applicationContext)
+    }
 
     open val userManager by lazy {
         UserManager(SharedPreferencesStorage(this))
